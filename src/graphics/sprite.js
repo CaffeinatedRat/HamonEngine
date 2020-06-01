01ccf03aadfd5a408eceb13cf4f3b4c0ac677bdc
CaffeinatedRat <caffeinatedrat@gmail.com>
2020-05-31 23:22:55 -0400

    Moved the sprite wrapping logic into its own method.

--- src/graphics/sprite.js
+++ src/graphics/sprite.js
@@ -27,7 +27,7 @@
 
 hamonengine.graphics = hamonengine.graphics || {};
 
-(function() {
+(function () {
 
     const SPRITE_ORIENTATION = {
         NORMAL: 0,
@@ -55,8 +55,8 @@ hamonengine.graphics = hamonengine.graphics || {};
 
             //Transformation variables.
             this._theta = options.theta || 0.0;
-            this._scaleVector = new hamonengine.geometry.vector2(1.0,1.0);
-            
+            this._scaleVector = new hamonengine.geometry.vector2(1.0, 1.0);
+
             this._spriteOrientation = SPRITE_ORIENTATION.NORMAL;
             this._showDiagnosisLines = false;
             this._showFullImage = false;
@@ -173,8 +173,8 @@ hamonengine.graphics = hamonengine.graphics || {};
          * @param {number} a alpha channel ranging from 0-255.
          * @param {number} blendingOps (BLENDING_OPS) blending operation to perform.
          */
-        blendColor(r=0, g=0, b=0, a=0, blendingOps) {
-            this._image.blendColorRegion(r,g,b,a, this._dimensions, blendingOps);
+        blendColor(r = 0, g = 0, b = 0, a = 0, blendingOps) {
+            this._image.blendColorRegion(r, g, b, a, this._dimensions, blendingOps);
         }
         /**
          * Adjusts the channels for each color.
@@ -183,15 +183,15 @@ hamonengine.graphics = hamonengine.graphics || {};
          * @param {number} b blue channel ranging from 0.0-1.0.
          * @param {number} a alpha channel ranging from 0.0-1.0.
          */
-        adjustColorChannel(r=1.0, g=1.0, b=1.0, a=1.0) {
-            this._image.adjustColorChannel(r,g,b,a, this._dimensions);
+        adjustColorChannel(r = 1.0, g = 1.0, b = 1.0, a = 1.0) {
+            this._image.adjustColorChannel(r, g, b, a, this._dimensions);
         }
         /**
          * Performs a bitblit between two sprites where this is the destination and the source sprite is passed in.
          * @param {*} sprite to bitblit with.
          * @param {number} transparency the intensity of the pixel's transparency. 0.0 - 1.0 where 0 is transparent and 1 is opaque.
          */
-        bitblit(sprite, transparency=1.0) {
+        bitblit(sprite, transparency = 1.0) {
             this._image.bitblit(sprite._image, sprite._dimensions, this._dimensions, transparency);
         }
         /**
@@ -203,7 +203,7 @@ hamonengine.graphics = hamonengine.graphics || {};
          * @param {number} width the optional width of the sprite to scale.
          * @param {number} height the option height of the sprite to scale.
          */
-        drawRaw(layer, elapsedTimeInMilliseconds, x, y, width=this._dimensions.width, height=this._dimensions.height) {
+        drawRaw(layer, elapsedTimeInMilliseconds, x, y, width = this._dimensions.width, height = this._dimensions.height) {
             if (this._image.complete) {
                 if (this.showFullImage) {
                     layer.context.drawImage(this._image.image,
@@ -238,15 +238,15 @@ hamonengine.graphics = hamonengine.graphics || {};
          * @param {number} width the optional width of the sprite.
          * @param {number} height the option height of the sprite.
          */
-        draw(layer, elapsedTimeInMilliseconds, x, y, width=this._dimensions.width, height=this._dimensions.height) {
+        draw(layer, elapsedTimeInMilliseconds, x, y, width = this._dimensions.width, height = this._dimensions.height) {
 
             //Round x,y coordinates into integers.
             x = Math.bitRound(x);
             y = Math.bitRound(y);
 
             //Find the center of the sprite.
-            let xCenterOffset = Math.bitRound(width/2) + x;
-            let yCenterOffset = Math.bitRound(height/2) + y;
+            let xCenterOffset = Math.bitRound(width / 2) + x;
+            let yCenterOffset = Math.bitRound(height / 2) + y;
 
             //Precalculate the orientation around the x & y axis.
             let yOrientation = hamonengine.util.bitwise.isSet(this._spriteOrientation, SPRITE_ORIENTATION.FLIPPED) ? -1.0 : 1.0;
@@ -254,7 +254,7 @@ hamonengine.graphics = hamonengine.graphics || {};
 
             //Save the current state.
             layer.save();
- 
+
             //Scale the sprite.
             if (this._scaleVector.x !== 1 || this._scaleVector.y !== 1) {
                 layer.context.translate(x, y);
@@ -285,85 +285,100 @@ hamonengine.graphics = hamonengine.graphics || {};
 
             //Sprite wrapping relative to the local sprite's transformation not to world transformations.
             if (layer.wrapHorizontal || layer.wrapVertical) {
-                let cosAngle = Math.cos(-this.theta);
-                let sinAngle = Math.sin(-this.theta);
+                this.drawSpriteWrapping(layer, elapsedTimeInMilliseconds, x, y, width, height, xOrientation, yOrientation);
+            }
 
-                let widthScaled = width * this._scaleVector.x;
-                let heightScaled = height * this._scaleVector.y;
+            //Restore the previous state.
+            layer.restore();
+        }
+        /**
+         * Draws & handles the sprite wrapping.  This is more of an internal method.
+         * @param {object} layer to draw upon.
+         * @param {number} elapsedTimeInMilliseconds the time elapsed between frames in milliseconds.
+         * @param {number} x coordinate to draw at.
+         * @param {number} y cooridnate to draw at.
+         * @param {number} width the width of the sprite.
+         * @param {number} height the height of the sprite.
+         * @param {number} xOrientation the xOrientation of the sprite.
+         * @param {number} yOrientation the yOrientation of the sprite.
+         */
+        drawSpriteWrapping(layer, elapsedTimeInMilliseconds, x, y, width, height, xOrientation, yOrientation) {
 
-                //Horizontal wrapping.
-                if (layer.wrapHorizontal) {
-                    //Inversely scale the length to the size of the sprite scaled horizontally relative to its local transformation.
-                    //For example, if the sprite has been shrunk by half, then the length must be doubled.
-                    let horizontalLength = layer.viewPort.width / this._scaleVector.x * xOrientation;
+            let cosAngle = Math.cos(-this.theta);
+            let sinAngle = Math.sin(-this.theta);
 
-                    //Rotate and scale the horizontal vector to keep our wrapping sprite on a horizontal plane with canvas not with the rotation plane so it appears at the edges of the screen.
-                    let v1x = Math.bitRound(cosAngle * horizontalLength);
-                    let v1y = Math.bitRound(sinAngle * horizontalLength);
-                    if (x - widthScaled <= layer.viewPort.x) {
-                        //Keep wrapping until we reach the max condition.
-                        let wrapLoop = Math.abs(parseInt((x-widthScaled)/layer.viewPort.width)) + 1;
-                        if (this._maxWrapping === 0 || wrapLoop < this._maxWrapping) {
-                            //P1: Draw an orthogonally RIGHT sprite relative to the world transformation [0 radians]
+            let widthScaled = width * this._scaleVector.x;
+            let heightScaled = height * this._scaleVector.y;
+
+            //Horizontal wrapping.
+            if (layer.wrapHorizontal) {
+                //Inversely scale the length to the size of the sprite scaled horizontally relative to its local transformation.
+                //For example, if the sprite has been shrunk by half, then the length must be doubled.
+                let horizontalLength = layer.viewPort.width / this._scaleVector.x * xOrientation;
+
+                //Rotate and scale the horizontal vector to keep our wrapping sprite on a horizontal plane with canvas not with the rotation plane so it appears at the edges of the screen.
+                let v1x = Math.bitRound(cosAngle * horizontalLength);
+                let v1y = Math.bitRound(sinAngle * horizontalLength);
+                if (x - widthScaled <= layer.viewPort.x) {
+                    //Keep wrapping until we reach the max condition.
+                    let wrapLoop = Math.abs(parseInt((x - widthScaled) / layer.viewPort.width)) + 1;
+                    if (this._maxWrapping === 0 || wrapLoop < this._maxWrapping) {
+                        //P1: Draw an orthogonally RIGHT sprite relative to the world transformation [0 radians]
+                        this.drawRaw(layer, elapsedTimeInMilliseconds, x + v1x * wrapLoop, y + v1y * wrapLoop, width, height);
+                        //Need to create continuous wrapping, where the sprite occupies both sides of the screen simultaneously.
+                        if (--wrapLoop > 0) {
                             this.drawRaw(layer, elapsedTimeInMilliseconds, x + v1x * wrapLoop, y + v1y * wrapLoop, width, height);
-                            //Need to create continuous wrapping, where the sprite occupies both sides of the screen simultaneously.
-                            if (--wrapLoop > 0) {
-                                this.drawRaw(layer, elapsedTimeInMilliseconds, x + v1x * wrapLoop, y + v1y * wrapLoop, width, height);
-                            }
-                        }
-                    }
-                    else if (x + widthScaled >= layer.viewPort.width) {
-                        let wrapLoop = parseInt((x+widthScaled)/layer.viewPort.width);
-                        //Keep wrapping until we reach the map condition.
-                        if (this._maxWrapping === 0 || wrapLoop < this._maxWrapping) {
-                            //P3: Draw an orthogonally LEFT sprite relative to the world transformation [PI radians]
-                            this.drawRaw(layer, elapsedTimeInMilliseconds, x - v1x * wrapLoop, y - v1y * wrapLoop, width, height);
-                            //Need to create continuous wrapping, where the sprite occupies both sides of the screen simultaneously.
-                            if (--wrapLoop > 0) {
-                                this.drawRaw(layer, elapsedTimeInMilliseconds, x - v1x * wrapLoop, y - v1y * wrapLoop, width, height);
-                            }
                         }
                     }
                 }
-
-                //Vertical wrapping.
-                if (layer.wrapVertical) {
-                    //Inversely scale the length to the size of the sprite scaled vertically relative to its local transformation.
-                    //For example, if the sprite has been shrunk by half, then the length must be doubled.
-                    let verticalLength = layer.viewPort.height / this._scaleVector.y * yOrientation;
-
-                    //Rotate and scale the vertical vector to keep our wrapping sprite on a vertical plane with canvas not with the rotation plane so it appears at the edges of the screen.
-                    let v2x = Math.bitRound(sinAngle * verticalLength);
-                    let v2y = Math.bitRound(cosAngle * verticalLength);
-                    if (y - heightScaled  <= layer.viewPort.y) {
-                        //Keep wrapping until we reach the max condition.
-                        let wrapLoop = Math.abs(parseInt((y - heightScaled) /layer.viewPort.height)) + 1;
-                        if (this._maxWrapping === 0 || wrapLoop < this._maxWrapping) {
-                            //P2: Draw an orthogonally DOWN sprite relative to the world transformation [PI/2 radians] (canvas y coordinates are inverted with +y down)
-                            this.drawRaw(layer, elapsedTimeInMilliseconds, x - v2x * wrapLoop, y + v2y * wrapLoop, width, height);
-                            //Need to create continuous wrapping, where the sprite occupies both sides of the screen simultaneously.
-                            if (--wrapLoop > 0) {
-                                this.drawRaw(layer, elapsedTimeInMilliseconds, x - v2x * wrapLoop, y + v2y * wrapLoop, width, height);
-                            }
-                        }
-                    }
-                    else if (y + heightScaled >= layer.viewPort.height) {
-                        let wrapLoop = parseInt((y + heightScaled) / layer.viewPort.height);
-                        //Keep wrapping until we reach the map condition.
-                        if (this._maxWrapping === 0 || wrapLoop < this._maxWrapping) {
-                            //P4: Draw an orthogonally UP sprite relative to the world transformation [3PI/2 radians] (canvas y coordinates are inverted with -y up)
-                            this.drawRaw(layer, elapsedTimeInMilliseconds, x + v2x * wrapLoop, y - v2y * wrapLoop, width, height);
-                            //Need to create continuous wrapping, where the sprite occupies both sides of the screen simultaneously.
-                            if (--wrapLoop > 0) {
-                                this.drawRaw(layer, elapsedTimeInMilliseconds, x + v2x * wrapLoop, y - v2y * wrapLoop, width, height);
-                            }
+                else if (x + widthScaled >= layer.viewPort.width) {
+                    let wrapLoop = parseInt((x + widthScaled) / layer.viewPort.width);
+                    //Keep wrapping until we reach the map condition.
+                    if (this._maxWrapping === 0 || wrapLoop < this._maxWrapping) {
+                        //P3: Draw an orthogonally LEFT sprite relative to the world transformation [PI radians]
+                        this.drawRaw(layer, elapsedTimeInMilliseconds, x - v1x * wrapLoop, y - v1y * wrapLoop, width, height);
+                        //Need to create continuous wrapping, where the sprite occupies both sides of the screen simultaneously.
+                        if (--wrapLoop > 0) {
+                            this.drawRaw(layer, elapsedTimeInMilliseconds, x - v1x * wrapLoop, y - v1y * wrapLoop, width, height);
                         }
                     }
                 }
             }
 
-            //Restore the previous state.
-            layer.restore();
+            //Vertical wrapping.
+            if (layer.wrapVertical) {
+                //Inversely scale the length to the size of the sprite scaled vertically relative to its local transformation.
+                //For example, if the sprite has been shrunk by half, then the length must be doubled.
+                let verticalLength = layer.viewPort.height / this._scaleVector.y * yOrientation;
+
+                //Rotate and scale the vertical vector to keep our wrapping sprite on a vertical plane with canvas not with the rotation plane so it appears at the edges of the screen.
+                let v2x = Math.bitRound(sinAngle * verticalLength);
+                let v2y = Math.bitRound(cosAngle * verticalLength);
+                if (y - heightScaled <= layer.viewPort.y) {
+                    //Keep wrapping until we reach the max condition.
+                    let wrapLoop = Math.abs(parseInt((y - heightScaled) / layer.viewPort.height)) + 1;
+                    if (this._maxWrapping === 0 || wrapLoop < this._maxWrapping) {
+                        //P2: Draw an orthogonally DOWN sprite relative to the world transformation [PI/2 radians] (canvas y coordinates are inverted with +y down)
+                        this.drawRaw(layer, elapsedTimeInMilliseconds, x - v2x * wrapLoop, y + v2y * wrapLoop, width, height);
+                        //Need to create continuous wrapping, where the sprite occupies both sides of the screen simultaneously.
+                        if (--wrapLoop > 0) {
+                            this.drawRaw(layer, elapsedTimeInMilliseconds, x - v2x * wrapLoop, y + v2y * wrapLoop, width, height);
+                        }
+                    }
+                }
+                else if (y + heightScaled >= layer.viewPort.height) {
+                    let wrapLoop = parseInt((y + heightScaled) / layer.viewPort.height);
+                    //Keep wrapping until we reach the map condition.
+                    if (this._maxWrapping === 0 || wrapLoop < this._maxWrapping) {
+                        //P4: Draw an orthogonally UP sprite relative to the world transformation [3PI/2 radians] (canvas y coordinates are inverted with -y up)
+                        this.drawRaw(layer, elapsedTimeInMilliseconds, x + v2x * wrapLoop, y - v2y * wrapLoop, width, height);
+                        //Need to create continuous wrapping, where the sprite occupies both sides of the screen simultaneously.
+                        if (--wrapLoop > 0) {
+                            this.drawRaw(layer, elapsedTimeInMilliseconds, x + v2x * wrapLoop, y - v2y * wrapLoop, width, height);
+                        }
+                    }
+                }
+            }
         }
         /**
          * Draws the diagnosis outlines.  This is more of an internal method.
@@ -377,8 +392,8 @@ hamonengine.graphics = hamonengine.graphics || {};
         drawDiagnosisOutlines(layer, elapsedTimeInMilliseconds, x, y, width, height) {
 
             //Find the center of the sprite.
-            let xCenterOffset = Math.bitRound(width/2) + x;
-            let yCenterOffset = Math.bitRound(height/2) + y;
+            let xCenterOffset = Math.bitRound(width / 2) + x;
+            let yCenterOffset = Math.bitRound(height / 2) + y;
 
             //Shows the wrapping test pattern.
             if (layer.wrapHorizontal || layer.wrapVertical) {
