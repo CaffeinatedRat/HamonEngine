3aa014b28bee87a26cc372d1d0398070f0c583a1
Ken Anderson <caffeinatedrat@gmail.com>
2023-02-04 18:51:05 -0500

    Build & tools update

--- tools/BuildScript.bat
+++ tools/BuildScript.bat
@@ -6,9 +6,9 @@ java -jar "tools/closure-compiler-v20200719.jar" --output_manifest "build/manife
  --charset UTF-8 --language_in=ES_2020^
  --js_output_file="build/hamonengine.min.js" --js="src/global.js"^
  --js="src/util/*.js"^
- --js="src/math/*.js"^
- --js="src/audio/*.js"^
+ --js="src/math/*.js" --js="src/math/datastructures/*.js"^
  --js="src/geometry/*.js"^
+ --js="src/audio/*.js"^
  --js="src/graphics/type.js" --js="src/graphics/imageext.js" --js="src/graphics/layer.js" --js="src/graphics/sprite.js" --js="src/graphics/animsprite.js" --js="src/graphics/spritesheet.js"^
  --js="src/entities/type.js" --js="src/entities/object2d.js" --js="src/entities/spriteObject.js" --js="src/entities/shapeObject.js" --js="src/entities/cell.js"^
  --js="src/events/*.js"^
